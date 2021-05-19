23193a8d3c6b4a89486dc51bc9c67c93785dc755
Yash Gupta <yashguptaz@protonmail.com>
2021-05-19 10:38:10 +0530

    Add Italic Keywords

--- lua/theming.lua
+++ lua/theming.lua
@@ -4,6 +4,7 @@ vim.cmd('syntax enable')
 vim.g.tokyonight_style = "night"
 vim.g.material_style = "deep ocean"
 vim.o.guifont = "FiraCode NF:h15"
+vim.g.material_italic_keywords = true
 
 require('material').set()
 
