8e215a174675e61a18044e40ac219f17417bc730
Yash Gupta <yashguptaz@protonmail.com>
2021-06-08 20:38:23 +0530

    Minor themeing.lua changes

--- lua/theming.lua
+++ lua/theming.lua
@@ -1,6 +1,5 @@
 vim.cmd('set termguicolors')
 vim.cmd('syntax enable')
-Hamari Adhuri Kahani
 vim.g.tokyonight_style = "night"
 vim.g.material_style = "deep ocean"
 vim.o.guifont = "FiraCode NF:h15"
@@ -9,6 +8,4 @@ vim.g.material_italic_keywords = true
 require('material').set()
 
 -- Switch To Lighter Theme with :Lighter
-vim.cmd(":command LightMaterial lua require('material.functions').change_style('lighter')<CR>")
-
-return M
+vim.cmd(":command LightTheme lua require('material.functions').change_style('lighter')<CR>")
